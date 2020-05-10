853bf2f9e331d6548c12a0d31f8c660bc9145ba5
Dawid Kędzierski <kedzierskidawid@MacBook-Pro-Dawid.local>
2020-05-10 14:33:48 +0200

    Initial commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 @Module({
   imports: [
     UsersModule,
-    MongooseModule.forRoot(`mongodb+srv://kedzierskidawid:uPUgElpNZVwAfpRV@kedzierskidawid-smyrj.mongodb.net/test?retryWrites=true&w=majority`, {
+    MongooseModule.forRoot(`mongodb+srv://kedzierskidawid:PASSWORD@kedzierskidawid-smyrj.mongodb.net/test?retryWrites=true&w=majority`, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       dbName: 'example'
